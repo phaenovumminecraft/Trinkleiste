7448d29170efeb82de30b7c8bdc81cbc89ff436e
Felix Joeken <felix@joeken.eu>
2019-04-11 15:40:52 +0200

    added xp ticker

--- src/eu/phaenovum/it/minecraft/trinkleiste/Main.java
+++ src/eu/phaenovum/it/minecraft/trinkleiste/Main.java
@@ -1,6 +1,7 @@
 package eu.phaenovum.it.minecraft.trinkleiste;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
