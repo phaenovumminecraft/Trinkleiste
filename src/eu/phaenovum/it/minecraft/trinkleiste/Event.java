68d76c4d854157a2a4b35d503fa4dc3212778eaa
Felix Joeken <felix@joeken.eu>
2019-04-11 15:27:52 +0200

    added example Event

---
+++ src/eu/phaenovum/it/minecraft/trinkleiste/Event.java
@@ -0,0 +1,14 @@
+package eu.phaenovum.it.minecraft.trinkleiste;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDamageEvent;
+
+public class Event implements Listener {
+
+    @EventHandler
+    public void exampleEvent(BlockDamageEvent event) {
+
+    }
+
+}
