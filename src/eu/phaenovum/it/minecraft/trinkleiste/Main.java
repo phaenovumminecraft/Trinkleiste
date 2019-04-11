7939fcd08123184c576a3a8a0cc85579e539dd48
Felix Joeken <felix@joeken.eu>
2019-04-11 16:00:25 +0200

    added xp ticker

--- src/eu/phaenovum/it/minecraft/trinkleiste/Main.java
+++ src/eu/phaenovum/it/minecraft/trinkleiste/Main.java
@@ -1,6 +1,7 @@
 package eu.phaenovum.it.minecraft.trinkleiste;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -8,5 +9,12 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(new Event(), this);
+        Bukkit.getScheduler().runTaskTimer(this, () -> {
+
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                p.setExp(p.getExp() + 5);
+            }
+
+        }, 200, 200);
     }
 }
